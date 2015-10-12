5a5fa5b8a95edd70b0403c266e1d87621b920870
Javier Rebollo Cinta <Javier@MacBook-Pro-de-Javier.local>
2015-10-12 21:25:59 +0200

    Commit with intellij

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-var express = require("express"),  
-    app = express(),
-    bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
-    mongoose = require('mongoose');
+var express  = require("express"),
+    app      = express(),
+    http     = require("http"),
+    server   = http.createServer(app),
+    mongoose = require('mongoose'); 
 
-app.use(bodyParser.urlencoded({ extended: false }));  
-app.use(bodyParser.json());  
-app.use(methodOverride());
+//app.configure(function () {
+//  app.use(express.bodyParser());
+//  app.use(express.methodOverride());
+//  app.use(app.router);
+//});
 
-var router = express.Router();
-
-router.get('/', function(req, res) {  
-   res.send("Hello World!");
+app.get('/', function(req, res) {
+  res.send("Hello world!");
 });
 
-app.use(router);
+routes = require('./routes/tvshows')(app);
 
-mongoose.connect('mongodb://localhost/tvshows', function(err, res) {  
-  if(err) {
-    console.log('ERROR: connecting to Database. ' + err);
-  }
-  app.listen(3000, function() {
-    console.log("Node server running on http://localhost:3000");
-  });
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+	if(err) {
+		console.log('ERROR: connecting to Database. ' + err);
+	} else {
+		console.log('Connected to Database');
+	}
+});
+
+server.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
 });
